0e39de086927de738f7781a5bec7e08a01b96201
Adam Chasen <adam@chasen.name>
2021-04-30 23:19:46 -0400

    edge and seven segment passing, frequency_counter test errors

--- src/frequency_counter.v
+++ src/frequency_counter.v
@@ -3,7 +3,9 @@
 module frequency_counter #(
     // If a module starts with #() then it is parametisable. It can be instantiated with different settings
     // for the localparams defined here. So the default is an UPDATE_PERIOD of 1200 and BITS = 12
-    localparam UPDATE_PERIOD = 1200,
+    //
+    // '-1' in the main, but not in the start
+    localparam UPDATE_PERIOD = 1200 - 1,
     localparam BITS = 12
 )(
     input wire              clk,
@@ -24,31 +26,81 @@ module frequency_counter #(
 
     reg [2:0] state = STATE_COUNT;
 
+    reg [3:0] tens;
+    reg [3:0] units;
+    reg load;
+    wire leading_edge_detect;
+
+    reg [BITS-1:0] sample;
+    reg [6:0] counter; //limit do 7 bits because we can only display up to 99 (2 digits)
+
+    seven_segment seven_segment0(.clk(clk), .reset(reset), .load, .ten_count(tens), .unit_count(units), .segments(segments), .digit(digit));
+    edge_detect edge_detect0(.clk(clk), .signal(signal), .leading_edge_detect(leading_edge_detect));
+
+    //always @(posedge clk) begin
+    //    if(reset)
+    //        update_period   <= UPDATE_PERIOD;
+    //    else if(period_load)
+    //        update_period   <= period;
+    //end
+
+
     always @(posedge clk) begin
         if(reset) begin
             
+            tens    <= 0;
+            units   <= 0;
+            load    <= 0; 
+            counter <= 0;
+            sample  <= 0;
+            state   <= STATE_COUNT;
             // reset things here
 
         end else begin
             case(state)
                 STATE_COUNT: begin
+                    // stop loading of new values to 7segment
+                    load <= 0;
+                    // could also do this in the if block which switches state
+                    tens    <= 0;
+                    units   <= 0;
+
                     // count edges and clock cycles
+                    sample <= sample + 1'b1;
+ 
+                    // can I get away with adding the detect value? NO per
+                    // simulation
+                    //counter <= counter + leading_edge_detect;
+                    if(leading_edge_detect)
+                        counter <= counter + 1'b1;
+
 
                     // if clock cycles > UPDATE_PERIOD then go to next state
+                    // if use a `begin` can clean up the counters here instead
+                    // of in STATE_UNITS
+                    if (sample >= UPDATE_PERIOD)
+                        state <= STATE_TENS;
                 end
 
                 STATE_TENS: begin
                     // count number of tens by subtracting 10 while edge counter >= 10
-
-                    // then go to next state
+                    if (counter < 7'd10)
+                        state <= STATE_UNITS;
+                    else
+                        tens <= tens + 1;
+                        counter <= counter - 7'd10;
                 end
 
                 STATE_UNITS: begin
                     // what is left in edge counter is units
-
+                    units <= counter; // not necessary to limit to bits [3:0];
                     // update the display
-
+                    load <= 1'b1;
                     // go back to counting
+                    state <= STATE_COUNT;
+                    // have to set these *before* we get to STATE_COUNT
+                    counter <= 0;
+                    sample  <= 0;
                 end
 
                 default:
--- src/seven_segment.v
+++ src/seven_segment.v
@@ -10,4 +10,43 @@ module seven_segment (
     output reg          digit
 );
 
+    reg [3:0] tens;
+    reg [3:0] units;
+    wire [3:0] decode;
+
+    always @(posedge clk) begin
+        if (reset) begin
+            tens <= 0;
+            units <= 0;
+            digit <= 0;
+        end else begin
+            if (load) begin
+                tens <= ten_count;
+                units <= unit_count;
+            end
+            // flip flop between segment cathode
+            digit <= ~digit;
+        end
+    end
+
+    assign decode = digit ? tens : units;
+
+    always @(*) begin
+        case(decode)
+            //                7654321
+            0:  segments = 7'b0111111;
+            1:  segments = 7'b0000110;
+            2:  segments = 7'b1011011;
+            3:  segments = 7'b1001111;
+            4:  segments = 7'b1100110;
+            5:  segments = 7'b1101101;
+            6:  segments = 7'b1111100;
+            7:  segments = 7'b0000111;
+            8:  segments = 7'b1111111;
+            9:  segments = 7'b1100111;
+            default:
+                segments = 7'b0000000;
+        endcase
+    end
+
 endmodule
