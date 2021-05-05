e62773b1bbeb2cfc9c575d8c9850a58443384dd7
Adam Chasen <adam@chasen.name>
2021-05-05 16:26:53 -0400

    invert sevensegment to support active low pmod

--- src/seven_segment.v
+++ src/seven_segment.v
@@ -13,6 +13,7 @@ module seven_segment (
     reg [3:0] tens;
     reg [3:0] units;
     wire [3:0] decode;
+    reg [6:0] segments_n;
 
     always @(posedge clk) begin
         if (reset) begin
@@ -30,23 +31,24 @@ module seven_segment (
     end
 
     assign decode = digit ? tens : units;
+    // invert segments because of 1bitsquared pmode active low
+    assign segments = segments_n ^ 7'b1111111;
 
     always @(*) begin
         case(decode)
             //                7654321
-            0:  segments = 7'b0111111;
-            1:  segments = 7'b0000110;
-            2:  segments = 7'b1011011;
-            3:  segments = 7'b1001111;
-            4:  segments = 7'b1100110;
-            5:  segments = 7'b1101101;
-            6:  segments = 7'b1111100;
-            7:  segments = 7'b0000111;
-            8:  segments = 7'b1111111;
-            9:  segments = 7'b1100111;
+            0:  segments_n = 7'b0111111;
+            1:  segments_n = 7'b0000110;
+            2:  segments_n = 7'b1011011;
+            3:  segments_n = 7'b1001111;
+            4:  segments_n = 7'b1100110;
+            5:  segments_n = 7'b1101101;
+            6:  segments_n = 7'b1111100;
+            7:  segments_n = 7'b0000111;
+            8:  segments_n = 7'b1111111;
+            9:  segments_n = 7'b1100111;
             default:
-                segments = 7'b0000000;
+                segments_n = 7'b0000000;
         endcase
     end
-
 endmodule
